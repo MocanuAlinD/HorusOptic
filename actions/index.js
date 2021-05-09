6191a903d80c5b8bdfbd5ff7ca06474dea08035a
MocanuAlinD <alin_ngt@yahoo.com>
2021-05-09 19:44:18 +0300

    Fixed footer color for mobile

--- actions/index.js
+++ actions/index.js
@@ -23,6 +23,29 @@ const INDEX_DATA = [
 
 ]
 
+const PRODUSE_RAME = [
+    {id: '1', name: 'Alin1', material: 'plastic', price: '152'},
+    {id: '2', name: 'Alin2', material: 'organic', price: '335'},
+    {id: '3', name: 'Alin3', material: 'plastic', price: '363'},
+    {id: '4', name: 'Alin4', material: 'metal', price: '583'},
+    {id: '5', name: 'Alin5', material: 'plastic', price: '641'},
+    {id: '6', name: 'Alin6', material: 'organic', price: '111'},
+    {id: '7', name: 'Alin7', material: 'metal', price: '913'},
+    {id: '8', name: 'Alin8', material: 'plastic', price: '767'},
+    {id: '9', name: 'Alin9', material: 'plastic', price: '193'},
+    {id: '10', name: 'Alin10', material: 'plastic', price: '557'},
+    {id: '11', name: 'Alin11', material: 'metal', price: '935'},
+    {id: '12', name: 'Alin12', material: 'plastic', price: '322'},
+    {id: '13', name: 'Alin13', material: 'plastic', price: '458'},
+    {id: '14', name: 'Alin14', material: 'plastic', price: '679'},
+    {id: '15', name: 'Alin15', material: 'metal', price: '788'},
+    {id: '16', name: 'Alin16', material: 'plastic', price: '640'},
+    {id: '17', name: 'Alin17', material: 'plastic', price: '915'},
+    {id: '18', name: 'Alin18', material: 'organic', price: '511'},
+    {id: '19', name: 'Alin19', material: 'plastic', price: '762'},
+    {id: '20', name: 'Alin20', material: 'plastic', price: '906'},
+]
+
 
 export const getIndexData = () => {
     return new Promise((resolve, reject) => {
---
+++ pages/Produse/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../../styles/Produse.module.css'
+
+const Produse = () => {
+    return (
+        <div className={styles.container}>
+            
+        </div>
+    );
+}
+
+export default Produse;
