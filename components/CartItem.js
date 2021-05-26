a037502be91a668f11686ef69a3d0826c1b26dba
MocanuAlinD <alin_ngt@yahoo.com>
2021-05-26 17:27:50 +0300

    Cart style OK mobile desktop

--- components/CartItem.js
+++ components/CartItem.js
@@ -8,18 +8,22 @@ const CartItem = ({ item, onUpdateCartQty, onRemoveFromCart}) => {
 
     return (
         <div key={item.id} className={styles.container}>
-            <Image priority='true' layout='responsive' as='image' src={item.media.source} width={1920} height={1080} />
-            <div className={styles.namePrice}>
-                <h4 className={styles.name}>{item.name}</h4>
-                <h4 className={styles.price}>{item.price.raw * item.quantity} <sub>ron</sub></h4>
+            <div className={styles.img}>
+                <Image priority='true' layout='responsive' as='image' src={item.media.source} width={1920} height={1080} />
             </div>
-            <div className={styles.buttons}>
-                <div className={styles.plusMinus}>
-                    <button onClick={() => onUpdateCartQty(item.id, item.quantity - 1)}>-</button>
-                    <h5>{item.quantity}</h5>
-                    <button onClick={() => onUpdateCartQty(item.id, item.quantity + 1)}>+</button>
+            <div className={styles.textButtons}>
+                <div className={styles.namePrice}>
+                    <h4 className={styles.name}>{item.name}</h4>
+                    <h4 className={styles.price}>{item.price.raw * item.quantity} <sub>ron</sub></h4>
                 </div>
-                <button className={styles.sterge} onClick={() => onRemoveFromCart(item.id)}>Sterge</button>
+                <div className={styles.buttons}>
+                    <div className={styles.plusMinus}>
+                        <button onClick={() => onUpdateCartQty(item.id, item.quantity - 1)}>-</button>
+                        <h5>{item.quantity}</h5>
+                        <button onClick={() => onUpdateCartQty(item.id, item.quantity + 1)}>+</button>
+                    </div>
+                    <button className={styles.sterge} onClick={() => onRemoveFromCart(item.id)}>Sterge</button>
+                </div>
             </div>
         </div>
     );
--- pages/Cart.js
+++ pages/Cart.js
@@ -13,24 +13,32 @@ const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart
 
     return (
         <div className={styles.cartContainer}>
-            <div className={styles.wrapperLeft}>
-                {noOfItems ? noOfItems.map(item => (
-                    <div key={item.id} className="">
-                        <CartItem item={item}
-                            onUpdateCartQty={handleUpdateCartQty}
-                            onRemoveFromCart={handleRemoveFromCart} />
-                    </div>
-                )) : ''}
-                {cart.total_items !== 0 ? <div className={styles.wrapperRight}>
-                    <h4>Subtotal: {cart.subtotal && cart.subtotal.formatted_with_code}</h4>
-                    <button className={styles.btnEmpty} onClick={() => handleEmptyCart()}>Goleste cosul</button>
-                    <button className={styles.btnCheckout}>Plateste</button>
-                </div> : <div className={styles.wrapperRight}>
-                    <h4>Nu aveti produse in cos. <Link href='/Produse'><a>Aici</a></Link> vedeti ce va oferim.</h4>
-                </div>}
+            <div className={styles.wrapper}>
+
+                <div className={styles.sus}>
+                    {noOfItems ? noOfItems.map(item => (
+                        <div key={item.id} className="">
+                            <CartItem item={item}
+                                onUpdateCartQty={handleUpdateCartQty}
+                                onRemoveFromCart={handleRemoveFromCart} />
+                        </div>
+                    )) : ''}
+                </div>
+
+
+                <div className={styles.jos}>
+                    {cart.total_items !== 0 ? <div className={styles.wrapperRight}>
+                        <h4>Subtotal: {cart.subtotal && cart.subtotal.formatted_with_code}</h4>
+                        <div className={styles.endButtons}>
+                            <button className={styles.btnEmpty} onClick={() => handleEmptyCart()}>Goleste cosul</button>
+                            <button className={styles.btnCheckout}>Plateste</button>
+                        </div>
+                    </div> : <div className={styles.emptyCart}>
+                        <h5>Nu aveti produse in cos. <Link href='/Produse'><a>Aici</a></Link> vedeti produsele noastre.</h5>
+                    </div>}
+                </div>
+
             </div>
-
-
         </div>
     );
 }
