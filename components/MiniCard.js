e0d060520c1464e8e258eeb5f874ad89f7038da2
MocanuAlinD <alin_ngt@yahoo.com>
2021-05-30 12:06:56 +0300

    Quantity not responsive 02

--- components/MiniCard.js
+++ components/MiniCard.js
@@ -29,7 +29,7 @@ const MiniCard = ({ produs, change, onAddToCart }) => {
             <hr className={styles.divider} />
             <h5>In stoc: <span>{cant}</span> </h5>
             {/* <div className={styles.link}><button onClick={() => onAddToCart(produs.id, 1)} disabled={true && produs.inventory.available < 99}>Adauga in cos</button></div> */}
-            {/* <button onClick={()=>getme()}>Click me</button> */}
+            <button onClick={()=>getme()}>Click me</button>
 
 
 
@@ -37,7 +37,7 @@ const MiniCard = ({ produs, change, onAddToCart }) => {
 
 
             {/* <div className={styles.link}><button onClick={() => onAddToCart(produs.id, 1)} disabled={true && produs.inventory.available < 99}>Adauga in cos</button></div> */}
-            {produs.inventory.available < 98 ?
+            {produs.inventory.available < 90 ?
                 (<div className={styles.link}><button title="Produsul nu este pe stoc. Comanda doar la telefon." disabled={true && produs.inventory.available < 99}>INDISPONIBIL</button></div>) :
                 (<div className={styles.link}><button onClick={() => onAddToCart(produs.id, 1)}>Adauga in cos</button></div>)
             }
