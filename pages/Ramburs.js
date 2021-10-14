c2fe022a5c58561f441ab5ffa6289525162dc111
MocanuAlinD <alin_ngt@yahoo.com>
2021-10-14 21:12:27 +0300

    Ramburs test 03

--- pages/Ramburs.js
+++ pages/Ramburs.js
@@ -1,38 +1,61 @@
 import React from "react";
 import styles from "../styles/Ramburs.module.css";
 import Link from "next/link";
+import Empty from "../components/Empty";
 
-const Ramburs = ({cart}) => {
-  console.log(cart.total_items)
-  console.log('alin\nmocanu')
-  if(cart.total_items === 0 || cart.total_items === undefined){
-    return <h1 style={{color:"white"}}>Nothing</h1>
+const Ramburs = ({ cart, onEmptyCart }) => {
+  // console.log(cart.total_items);
+  // console.log(cart);
+  if (cart.total_items === 0 || cart.total_items === undefined) {
+    return <Empty />;
   }
 
+  const removeCart = (e) => {
+    // console.log(e.target);
+    
+    setTimeout(() => {
+      onEmptyCart();
+      // console.log("remove cart");
+    }, 3000);
+    
+  };
 
   return (
     <div className={styles.ramburs__container}>
       <form
         action="https://formsubmit.co/81b98d1d934c3b7f0e32bc02eb386532"
         method="POST"
+        onSubmit={(e) => removeCart(e)}
       >
-        <input
-          type="text"
-          name="produse"
-          value={
-            cart.total_items !== 0 || cart.total_items !== undefined
-              ? cart.line_items.map(
-                  (item) =>
-                    item.media.source +
-                    "--" +
-                    item.quantity +
-                    "--" +
-                    item.price.formatted + '\n'
-                )
-              : "-"
-          }
+        <textarea
+          // style={{ display: "none" }}
+          name="Prods"
+          id=""
+          cols="30"
+          rows="10"
+          value={cart.line_items.map(
+            (item) =>
+              item.media.source +
+              "     " +
+              item.quantity +
+              "     " +
+              item.price.formatted +
+              "\n"
+          )}
           readOnly
-        />
+          required
+        >
+          {cart.line_items.map(
+            (item) =>
+              item.media.source +
+              "--" +
+              item.quantity +
+              "--" +
+              item.price.formatted +
+              "\n"
+          )}
+        </textarea>
+
         <input type="text" name="Nume" required placeholder="nume" />
         <input type="tel" name="Telefon" required placeholder="telefon" />
         <input type="email" name="Email" required placeholder="email" />
@@ -52,7 +75,7 @@ const Ramburs = ({cart}) => {
         <input
           type="hidden"
           name="_next"
-          value="http://localhost:3000/"
+          value="http://localhost:3000/Produse"
         ></input>
         <input type="hidden" name="_captcha" value="false"></input>
         <input type="hidden" name="_template" value="table"></input>
@@ -71,3 +94,23 @@ export default Ramburs;
 
 // cart.line_items.length;
 // https://formsubmit.co/81b98d1d934c3b7f0e32bc02eb386532
+
+/* <input
+          type="text"
+          name="produse"
+          value={
+            cart.total_items !== 0 || cart.total_items !== undefined
+              ? cart.line_items.map(
+                  (item) =>
+                    item.media.source +
+                    "--" +
+                    item.quantity +
+                    "--" +
+                    item.price.formatted +
+                    "\n"
+                )
+              : "-"
+          }
+          readOnly
+          // style={{display: "none"}}
+        /> */
