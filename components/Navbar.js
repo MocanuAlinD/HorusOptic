df96ccbc10bf622a25ef911ee3735786ad1cd26d
Mocanu Alin <alin_ngt@yahoo.com>
2022-07-26 16:20:28 +0300

    Fixed theme to localStorage

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
-import style from "../styles/navbar.module.css";
-import { GiEyeOfHorus } from "react-icons/gi";
+import styles from "../styles/navbar.module.css";
 import { BiSun, BiMoon } from "react-icons/bi";
-import { useState } from "react";
 import { IconButton, Badge } from "@material-ui/core";
 import { ShoppingCart } from "@material-ui/icons";
 import { useRouter } from "next/router";
@@ -11,7 +9,7 @@ import { useSession, signIn, signOut } from "next-auth/react";
 
 const Navbar = ({ totalItems }) => {
   const [state, setState] = useState(true);
-  const [themeIcon, setThemeIcon] = useState(true);
+  const [themeIcon, setThemeIcon] = useState(null);
   const router = useRouter();
 
   const { data: session } = useSession();
@@ -34,21 +32,57 @@ const Navbar = ({ totalItems }) => {
   };
 
   const changeTheme = () => {
-    document.body.classList.toggle("dark");
-    if (document.body.classList.contains("dark")) {
+    const item = localStorage.getItem("theme");
+    if (item === "false") {
+      document.body.classList.value = "dark";
+      localStorage.setItem("theme", true);
       setThemeIcon(false);
     } else {
+      document.body.classList.value = "";
+      localStorage.setItem("theme", false);
       setThemeIcon(true);
     }
   };
 
+  useEffect(() => {
+    try {
+      if (localStorage.getItem("theme") !== null) {
+        if (localStorage.getItem("theme") === "true") {
+          document.body.classList.value = "dark";
+          setThemeIcon(false);
+        } else if (localStorage.getItem("theme") === "false") {
+          document.body.classList.value = "";
+          setThemeIcon(true);
+        }
+      } else {
+        localStorage.setItem("theme", false);
+        document.body.classList.value = "";
+      }
+    } catch (error) {
+      console.log("No theme available.");
+    }
+  }, []);
+
   return (
-    <div className={style.nav} id="nav">
-      <div className={style.nav__title}>
-        <h3>HORUS TOP OPTIC</h3>
-        {/* <GiEyeOfHorus className={style.nav__icon} /> */}
+    <div
+      className={
+        styles.nav +
+        " m-0 p-0 d-flex justify-content-center align-items-center position-sticky top-0"
+      }
+      id="nav"
+    >
+      <div
+        className={
+          styles.nav__title +
+          " m-0 p-0 h-100 d-flex align-items-center ps-3 flex-grow-1"
+        }
+      >
+        <Link href="/">
+          {/* <h3 className="h-auto m-0 p-0">HORUS TOP OPTIC</h3> */}
+          <h3 className={styles.navTitle + ' h-auto m-0 p-0'}>HORUS TOP OPTIC</h3>
+        </Link>
       </div>
-      <ul>
+      <ul className="m-0 p-0 d-flex flex-grow-1 m-0 p-0 flex-column flex-md-row align-items-start justify-content-start justify-content-md-end gap-4 pt-5 pt-md-0 ">
         <li>
           <Link href="/">
             <a onClick={closeMenu}>Acasa</a>
@@ -72,7 +106,7 @@ const Navbar = ({ totalItems }) => {
 
         {session && (
           <li>
-            <img src={session.user.image} alt="" />
+            <img src={session.user.image} alt="user image" />
             <Link href="/">
               <a onClick={() => (signOut(), closeMenu())}>Log out</a>
             </Link>
@@ -86,14 +120,16 @@ const Navbar = ({ totalItems }) => {
           </li>
         )}
       </ul>
-      <button className={style.nav__menu} onClick={changeMe}>
-        Meniu
-      </button>
-      <div className={style.nav__cartContainer}>
+      <div className="">
+        <button className={styles.nav__menu + " m-0 w-auto"} onClick={changeMe}>
+          Meniu
+        </button>
+      </div>
+      <div className={styles.nav__cartContainer}>
         {router.pathname !== "/Cart" && router.pathname !== "/Checkout" && (
           <Link href="/Cart">
             <IconButton
-              className={style.nav__cartIcon}
+              className={styles.nav__cartIcon}
               aria-label="Show cart items"
               title="Vezi produsele din cos"
             >
@@ -110,14 +146,14 @@ const Navbar = ({ totalItems }) => {
       </div>
 
       <div
-        className={style.theme_color_container}
+        className={styles.theme_color_container}
         onClick={() => changeTheme()}
       >
-        <div className={style.sun}>
+        <div className={styles.sun}>
           {themeIcon === true ? (
-            <BiSun className={style.sunIcon} />
+            <BiSun className={styles.sunIcon} />
           ) : (
-            <BiMoon className={style.sunIcon} />
+            <BiMoon className={styles.sunIcon} />
           )}
         </div>
       </div>
